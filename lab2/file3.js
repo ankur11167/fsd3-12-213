f753766b1fbe4dd68813f74eed9dd68e204baf3b
ankur pandey <ankur29apr2007@gmail.com>
2026-09-18 14:37:59 +0530

    18th

--- lab2/file3.js
+++ lab2/file3.js
@@ -2,7 +2,7 @@ import { stat } from "fs/promises";
 
 const fstat = await stat("file1.js");
 
-console.log("filesize", fstat.size, "bytes");
+console.log("filesizee", fstat.size, "bytes");
 console.log(`is file: ${fstat.isFile()}`);
 console.log(`is folder: ${fstat.isDirectory()}`);
 console.log(`is symbolic link: ${fstat.isSymbolicLink()}`);
