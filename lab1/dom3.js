6b287d8160373ea9dca938bd123630f17335cf5b
ankur pandey <ankur29apr2007@gmail.com>
2026-09-18 14:43:40 +0530

    commiit

--- lab1/dom3.js
+++ lab1/dom3.js
@@ -2,7 +2,7 @@ import { EventEmitter } from "events";
 
 const button = new EventEmitter();
 
-button.on("click",(uname)=>{
+button.on("clicck",(uname)=>{
     console.log(`button clicked by ${uname}`);
 });
 button.emit("click", "Raju");
